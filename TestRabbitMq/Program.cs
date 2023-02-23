efa8edc66af8d0305dc6a9f187f661eed44d55ae
abdohosny1 <make.y90@yahoo.com>
2023-02-23 11:48:30 +0200

    Add project files.

---
+++ TestRabbitMq/Program.cs
@@ -0,0 +1,22 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factor = new ConnectionFactory() { HostName = "localhost" };
+using (var connection = factor.CreateConnection())
+using (var channel =connection.CreateModel())
+{
+    channel.ExchangeDeclare(exchange: "Test",
+                            type: "diret",
+                            autoDelete: true,
+                            durable: false);
+
+    var message = "Hello abdo";
+    var body=Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish(exchange: "Test",
+                         routingKey: "TestLog",
+                         basicProperties: null,
+                         body: body);
+
+    Console.WriteLine($" sent message =  {message}");
+}
